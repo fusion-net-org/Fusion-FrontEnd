845eeae31d7b28b4f8a26adbf1d9ed2069135bba
cong-bang <13092k1lcbang@gmail.com>
2025-10-07 21:18:55 +0700

    feat: setup layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import { ToastContainer } from "react-toastify";
 import ScrollToTop from "./utils/ScrollToTop";
 import NotFound from "./pages/notfound/NotFound";
 import Register from "./pages/register/Register";
-import Company from "./pages/company/Company";
+import Company from "./pages/home/Company";
 import MainLayout from "./layouts/MainLayout/MainLayout";
+import Landing from "./pages/landing/Landing";
+import HomeLayout from "./layouts/HomeLayout/HomeLayout";
 
 function App() {
   return (
@@ -15,9 +16,13 @@ function App() {
       <ScrollToTop />
 
       <Routes>
-        {/* Route có layout */}
+        {/* Route main layout */}
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
+          <Route index element={<Landing />} />
+        </Route>
+
+        {/* Route home layout */}
+        <Route element={<HomeLayout />}>
           <Route path="/company" element={<Company />} />
         </Route>
         
---
+++ src/layouts/HomeLayout/HomeLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import NavLeft from "../../components/NavLeft/NavLeft";
+
+const HomeLayout = () => {
+  return (
+    <div className="flex min-h-screen">
+      <aside className="w-64 border-r">
+        <NavLeft />
+      </aside>
+
+      <main className="flex-1 p-6 overflow-y-auto">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+export default HomeLayout;
