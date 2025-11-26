e19e6f1c1f1f688c092470d5c1285466523668f3
cong-bang <13092k1lcbang@gmail.com>
2025-11-26 14:04:22 +0700

    #197 add css company layout

--- src/layouts/Company/CompanyLayout.tsx
+++ src/layouts/Company/CompanyLayout.tsx
@@ -5,6 +5,7 @@ import { Outlet, useLocation, useParams } from 'react-router-dom';
 import CompanyHeader from './CompanyHeader';
 import CompanyNavbar from './CompanyNavbar';
 import CompanyFooter from './CompanyFooter';
+import './css/company-layout.css';
 import { setCurrentCompanyId, clearCurrentCompanyId } from '@/apiConfig.js';
 import { PermissionProvider } from '@/permission/PermissionProvider';
 import { getUserIdFromToken } from '@/utils/token';
@@ -31,22 +32,18 @@ export default function CompanyLayout({ children, initialTall = true }: Props) {
   }, [companyId]);
 
   return (
-    <div className="flex min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-100 transition-colors duration-500">
-      {/* LEFT SIDEBAR */}
+    <div className="cmp-theme flex min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-100 transition-colors duration-500">
       <div className="flex-shrink-0">
         <CompanyNavbar />
       </div>
 
-      {/* RIGHT: HEADER + CONTENT */}
       <div className="flex flex-col flex-1">
-        {/* HEADER */}
         <CompanyHeader />
 
-        {/* CONTENT */}
         <PermissionProvider key={permKey} userId={userId ?? ''} companyId={companyId as string}>
           <main
             key={fadeKey}
-            className={`flex-1 px-3 pb-6 overflow-y-auto bg-white dark:bg-gray-800
+            className={`cmp-main cmp-pagefade flex-1 px-3 pb-6 overflow-y-auto bg-white dark:bg-gray-800
                         border-l-[2px] border-gray-200 dark:border-gray-700
                         transition-colors duration-500
                         ${initialTall ? 'min-h-[calc(100vh-56px)]' : ''}`}
