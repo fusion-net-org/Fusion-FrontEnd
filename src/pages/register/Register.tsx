7760fa50aba06bc770e6e360970f1222026dda22
cong-bang <13092k1lcbang@gmail.com>
2025-11-29 14:46:57 +0700

    #225 update return error by string

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -36,9 +36,16 @@ const Register: React.FC = () => {
       }
     } catch (error: any) {
       if (error.errorData) {
-        const msgs = Object.values(error.errorData).flat() as string[];
-        msgs.forEach((msg) => toast.error(msg));
-        return;
+        if (typeof error.errorData === 'string') {
+          toast.error(error.errorData);
+          return;
+        }
+
+        if (typeof error.errorData === 'object') {
+          const msgs = Object.values(error.errorData).flat() as string[];
+          msgs.forEach((msg) => toast.error(msg));
+          return;
+        }
       }
       toast.error((error.message as string) || 'An error occurred while registering!');
     }
