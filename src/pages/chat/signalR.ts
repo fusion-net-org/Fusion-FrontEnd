c1b6e0159f4ddffb25b6f146a314bbfd626037a9
cong-bang <13092k1lcbang@gmail.com>
2026-01-12 19:44:39 +0700

    #309 add env

--- src/pages/chat/signalR.ts
+++ src/pages/chat/signalR.ts
@@ -1,7 +1,7 @@
 import * as signalR from '@microsoft/signalr';
 
 export const connection = new signalR.HubConnectionBuilder()
-  .withUrl('https://localhost:7160/chathub', {
+  .withUrl('https://www.tuongnt14.xyz/chathub', {
     accessTokenFactory: () => getAccessToken(),
   })
   .withAutomaticReconnect()
